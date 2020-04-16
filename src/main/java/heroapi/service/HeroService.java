abae9ac1db4fbe84a48298a5b3f9a0f2a43b6d7b
blee <bjl4271@gmail.com>
2020-04-15 20:13:19 -0400

    reformatted source code

--- src/main/java/heroapi/service/HeroService.java
+++ src/main/java/heroapi/service/HeroService.java
@@ -17,55 +17,53 @@ import heroapi.model.repository.HeroRepository;
 @Service
 public class HeroService {
 	private static final Logger logger = LoggerFactory.getLogger(HeroService.class);
-	
+
 	@Autowired
 	private HeroRepository heroRepo;
-	
+
 	public Hero createHero(APIHero apiHero) throws APIException {
-		if(apiHero.hero_id != null) {
+		if (apiHero.hero_id != null) {
 			throw new APIException("hero_id is auto-generated and cannot have a value");
 		}
-		
+
 		Hero hero = new Hero(apiHero.hero_name, apiHero.real_identity, apiHero.powers, apiHero.weaknesses);
 		heroRepo.save(hero);
 		logger.info("Created new hero: {} in database", hero.toString());
-		
+
 		return hero;
 	}
-	
+
 	public List<Hero> getHero(String name) {
 		List<Hero> heroList = new ArrayList<>();
-		
-		if(name == null || name.isBlank() || name.isEmpty()) {
+
+		if (name == null || name.isBlank() || name.isEmpty()) {
 			logger.info("Getting all heroes from database");
 			heroRepo.findAll().forEach(heroList::add);
-		}
-		else {
+		} else {
 			logger.info("Getting all heroes by name '{}' from database", name);
 			heroList.addAll(heroRepo.findByName(name));
 		}
 		return heroList;
 	}
-	
+
 	public Hero updateHero(String heroId, APIHero apiHero) throws APIException, ResourceNotFoundException {
-		if(heroId == null || heroId.isBlank()) {
+		if (heroId == null || heroId.isBlank()) {
 			throw new APIException("heroId missing from URL path");
 		}
-		
-		Hero hero = heroRepo.findById(Long.valueOf(heroId)).orElse(null);	
-		
-		if(hero == null) {
+
+		Hero hero = heroRepo.findById(Long.valueOf(heroId)).orElse(null);
+
+		if (hero == null) {
 			logger.error("hero_id: {} not found in database", heroId);
 			throw new ResourceNotFoundException("Hero with id: " + heroId + " not found in database");
-		}
-		else {
+		} else {
 			hero.setHeroName(apiHero.hero_name);
 			hero.setPowers(apiHero.powers);
 			hero.setWeaknesses(apiHero.weaknesses);
 			hero.setRealIdentity(apiHero.real_identity);
 			heroRepo.save(hero);
 		}
-		
+
 		return hero;
 	}
 }
--- src/test/java/heroapi/HeroAPIControllerTest.java
+++ src/test/java/heroapi/HeroAPIControllerTest.java
@@ -26,7 +26,7 @@ public class HeroAPIControllerTest {
 	private HeroAPIController heroController;
 	private HeroService heroServiceMock;
 	private VillainService villainServiceMock;
-	
+
 	@BeforeEach
 	public void setupControllerTests() {
 		heroServiceMock = mock(HeroService.class);
@@ -34,64 +34,56 @@ public class HeroAPIControllerTest {
 		heroController = new HeroAPIController(heroServiceMock, villainServiceMock);
 		mvc = MockMvcBuilders.standaloneSetup(heroController).build();
 	}
-	
-    @Test
-    public void test_welcome() throws Exception {
-        mvc.perform(get("/"))
-        	.andExpect(status().isOk())
-        	.andExpect(content().string(not(emptyOrNullString())));
-    }
-    
-    @Test
-    public void test_getHero() throws Exception {
-    	Hero hero = new Hero("Superman", "Clark Kent", "Super everything", "Kryptonite");
-    	
-    	when(heroServiceMock.getHero("Superman")).thenReturn(List.of(hero));
-    	
-    	mvc.perform(get("/hero?name=Superman"))
-    		.andExpect(status().isOk())
-    		.andExpect(jsonPath("$.data[0].heroName", equalTo(hero.getHeroName())))
-    		.andExpect(jsonPath("$.data[0].powers", equalTo(hero.getPowers())))
-    		.andExpect(jsonPath("$.data[0].weaknesses", equalTo(hero.getWeaknesses())))
-    		.andExpect(jsonPath("$.data[0].realIdentity", equalTo(hero.getRealIdentity())));
-    }
-    
-    @Test
-    public void test_getAllHeroes() throws Exception {
-    	List<Hero> heroList = List.of(new Hero("Superman", "Clark Kent", "Super everything", "Kryptonite"),
-    								new Hero("Batman", "Bruce Wayne", "Ace Detective", "None"));
-    	
-    	when(heroServiceMock.getHero(null)).thenReturn(heroList);
-    	
-    	mvc.perform(get("/hero"))
-    		.andExpect(status().isOk())
-    		.andExpect(jsonPath("$.data[*]", hasSize(2)));
-    }
-    
-    @Test
-    public void test_getVillain() throws Exception {
-    	Villain villain = new Villain("Captain Cold", "Leonard Snart", "Ice Control, Weapon Master", "Human");
-    	
-    	when(villainServiceMock.getVillain("Captain Cold")).thenReturn(List.of(villain));
-    	
-    	mvc.perform(get("/villain?name=Captain Cold"))
-    		.andExpect(status().isOk())
-    		.andExpect(jsonPath("$.data[0].villainName", equalTo(villain.getVillainName())))
-    		.andExpect(jsonPath("$.data[0].powers", equalTo(villain.getPowers())))
-    		.andExpect(jsonPath("$.data[0].weaknesses", equalTo(villain.getWeaknesses())))
-    		.andExpect(jsonPath("$.data[0].realIdentity", equalTo(villain.getRealIdentity())));
-    }
-    
-    @Test
-    public void test_getAllVillains() throws Exception {
-    	List<Villain> villainList = List.of(
-    				new Villain("Professor Zoom", "Eobard Thawne", "Negative Speed Force", "Insanity"),
-    				new Villain("Captain Cold", "Leonard Snart", "Ice Control, Weapon Master", "Human"));
-    	
-    	when(villainServiceMock.getVillain(null)).thenReturn(villainList);
-    	
-    	mvc.perform(get("/villain"))
-    		.andExpect(status().isOk())
-    		.andExpect(jsonPath("$.data[*]", hasSize(2)));
-    }
+
+	@Test
+	public void test_welcome() throws Exception {
+		mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(not(emptyOrNullString())));
+	}
+
+	@Test
+	public void test_getHero() throws Exception {
+		Hero hero = new Hero("Superman", "Clark Kent", "Super everything", "Kryptonite");
+
+		when(heroServiceMock.getHero("Superman")).thenReturn(List.of(hero));
+
+		mvc.perform(get("/hero?name=Superman")).andExpect(status().isOk())
+				.andExpect(jsonPath("$.data[0].heroName", equalTo(hero.getHeroName())))
+				.andExpect(jsonPath("$.data[0].powers", equalTo(hero.getPowers())))
+				.andExpect(jsonPath("$.data[0].weaknesses", equalTo(hero.getWeaknesses())))
+				.andExpect(jsonPath("$.data[0].realIdentity", equalTo(hero.getRealIdentity())));
+	}
+
+	@Test
+	public void test_getAllHeroes() throws Exception {
+		List<Hero> heroList = List.of(new Hero("Superman", "Clark Kent", "Super everything", "Kryptonite"),
+				new Hero("Batman", "Bruce Wayne", "Ace Detective", "None"));
+
+		when(heroServiceMock.getHero(null)).thenReturn(heroList);
+
+		mvc.perform(get("/hero")).andExpect(status().isOk()).andExpect(jsonPath("$.data[*]", hasSize(2)));
+	}
+
+	@Test
+	public void test_getVillain() throws Exception {
+		Villain villain = new Villain("Captain Cold", "Leonard Snart", "Ice Control, Weapon Master", "Human");
+
+		when(villainServiceMock.getVillain("Captain Cold")).thenReturn(List.of(villain));
+
+		mvc.perform(get("/villain?name=Captain Cold")).andExpect(status().isOk())
+				.andExpect(jsonPath("$.data[0].villainName", equalTo(villain.getVillainName())))
+				.andExpect(jsonPath("$.data[0].powers", equalTo(villain.getPowers())))
+				.andExpect(jsonPath("$.data[0].weaknesses", equalTo(villain.getWeaknesses())))
+				.andExpect(jsonPath("$.data[0].realIdentity", equalTo(villain.getRealIdentity())));
+	}
+
+	@Test
+	public void test_getAllVillains() throws Exception {
+		List<Villain> villainList = List.of(
+				new Villain("Professor Zoom", "Eobard Thawne", "Negative Speed Force", "Insanity"),
+				new Villain("Captain Cold", "Leonard Snart", "Ice Control, Weapon Master", "Human"));
+
+		when(villainServiceMock.getVillain(null)).thenReturn(villainList);
+
+		mvc.perform(get("/villain")).andExpect(status().isOk()).andExpect(jsonPath("$.data[*]", hasSize(2)));
+	}
 }
