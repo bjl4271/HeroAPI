d14c9689fc815483d185a820c019fd5b0879cb6a
blee <bjl4271@gmail.com>
2020-12-25 18:29:43 -0500

    fixed formatting

--- src/main/java/heroapi/model/api/APIHero.java
+++ src/main/java/heroapi/model/api/APIHero.java
@@ -1,19 +1,20 @@
 package heroapi.model.api;
 
 public class APIHero {
-	public Long hero_id;
-	public String hero_name;
-	public String real_identity;
-	public String powers;
-	public String weaknesses;
-	
-	public APIHero() {}
-	
-	public APIHero(Long id, String name, String identity, String powers, String weakness) {
-	    this.hero_id = id;
-	    this.hero_name = name;
-	    this.real_identity = identity;
-	    this.powers = powers;
-	    this.weaknesses = weakness;
-	}
+    public Long hero_id;
+    public String hero_name;
+    public String real_identity;
+    public String powers;
+    public String weaknesses;
+
+    public APIHero() {
+    }
+
+    public APIHero(Long id, String name, String identity, String powers, String weakness) {
+        this.hero_id = id;
+        this.hero_name = name;
+        this.real_identity = identity;
+        this.powers = powers;
+        this.weaknesses = weakness;
+    }
 }
--- src/main/java/heroapi/model/api/APIVillain.java
+++ src/main/java/heroapi/model/api/APIVillain.java
@@ -1,19 +1,20 @@
 package heroapi.model.api;
 
 public class APIVillain {
-	public Long villain_id;
-	public String villain_name;
-	public String real_identity;
-	public String powers;
-	public String weaknesses;
-	
-	public APIVillain() {}
-	
-	public APIVillain(Long id, String name, String identity, String powers, String weakness) {
-	    this.villain_id = id;
-	    this.villain_name = name;
-	    this.real_identity = identity;
-	    this.powers = powers;
-	    this.weaknesses = weakness;
-	}
+    public Long villain_id;
+    public String villain_name;
+    public String real_identity;
+    public String powers;
+    public String weaknesses;
+
+    public APIVillain() {
+    }
+
+    public APIVillain(Long id, String name, String identity, String powers, String weakness) {
+        this.villain_id = id;
+        this.villain_name = name;
+        this.real_identity = identity;
+        this.powers = powers;
+        this.weaknesses = weakness;
+    }
 }
